398c0e30ac9b9e611adc7e5ec9ccde0e2d0a237a
LuoQingy <1075899733@qq.com>
2019-10-31 18:08:03 +0800

    修改导航

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,17 +1,20 @@
 <template>
 	<view>
 		<mainpage v-if="PageCur=='mainpage'"></mainpage>
-		<good-friend-list v-if="PageCur=='discover'"></good-friend-list>
-		<message v-if="PageCur=='message'"></message>
 		<me v-if="PageCur=='me'"></me>
 		
 		<view class="cu-bar tabbar bg-white shadow foot">
-			<view class="action" :class="PageCur=='mainpage'?'new-text-blue':'new-text-black'" @click="NavChange" data-cur="mainpage" >
-				<view :class="PageCur=='mainpage'?'cuIcon-homefill':'cuIcon-home'"></view>
+			<view class="action" :class="PageCur=='discover'?'new-text-blue':'new-text-black'" @click="NavChange" data-cur="discover" >
+				<view :class="PageCur=='discover'?'cuIcon-friendfill':'cuIcon-friend'"></view>
 				<view >那些人</view>
+				<view class="new-text" :class="PageCur=='discover'?'active':''">
+					<view @tap="openMsg">文字匹配</view>
+					<view @tap="openMsg">语音匹配</view>
+					<text class="new-index-cuIcon"></text>
+				</view>
 			</view>
-			<view class="action " :class="PageCur=='discover'?'new-text-blue':'new-text-black'" @click="openMsg" data-cur="discover" >
-				<view :class="PageCur=='discover'?'cuIcon-messagefill':'cuIcon-message'"></view> 
+			<view class="action " :class="PageCur=='mainpage'?'new-text-blue':'new-text-black'" @click="NavChange" data-cur="mainpage" >
+				<view :class="PageCur=='mainpage'?'cuIcon-messagefill':'cuIcon-message'"></view> 
 				<view>那些话</view>
 			</view>
 			<view class="action" :class="PageCur=='errands'?'new-text-blue':'new-text-black'" @click="openRun" data-cur="errands" >
@@ -21,14 +24,17 @@
 				跑腿
 			</view>
 			<view class="action" :class="PageCur=='message'?'new-text-blue':'new-text-black'" @click="NavChange" data-cur="message" >
-				<view :class="PageCur=='message'?'cuIcon-shopfill':'cuIcon-shop'">
-					<!-- <view class="cu-tag badge">99</view> -->
+				<view :class="PageCur=='message'?'cuIcon-shopfill':'cuIcon-shop'"></view>
+				<view >商城</view>
+				<view class="new-shop new-text" :class="PageCur=='message'?'active':''">
+					<view>拍卖城</view>
+					<view>购物城</view>
+					<text class="new-index-cuIcon"></text>
 				</view>
-				商城
 			</view>
 			
 			<view class="action" :class="PageCur=='me'?'new-text-blue':'new-text-black'" @click="NavChange" data-cur="me" >
-				<view :class="PageCur=='me'?'cuIcon-formfill':'cuIcon-form'">
+				<view :class="PageCur=='me'?'cuIcon-circlefill':'cuIcon-circle'">
 					<!-- <view class="cu-tag badge"></view> -->
 				</view>
 				<view >分类</view>
@@ -36,29 +42,12 @@
 			
 		</view>
 		
-		<!-- <view class="cu-modal" :class="modalName=='RadioModal'?'show':''" @tap="hideModal">
-			<view class="cu-dialog" @tap.stop="">
-				<radio-group class="block" @change="RadioChange">
-					<view class="cu-list menu text-left">
-						<view class="cu-item" v-for="(item,index) in 5" :key="index">
-							<label class="flex justify-between align-center flex-sub">
-								<view class="flex-sub">Item {{index +1}}</view>
-								<radio class="round" :class="radio=='radio' + index?'checked':''" :checked="radio=='radio' + index?true:false"
-								 :value="'radio' + index"></radio>
-							</label>
-						</view>
-					</view>
-				</radio-group>
-			</view>
-		</view> -->
 		
 	</view>
 </template>
 
 <script>
-	import goodFriendList from '../message/good-friend-list.vue'
 	export default {
-		components: {goodFriendList},
 		data() {
 		return {
 				PageCur: 'mainpage',
@@ -79,10 +68,10 @@
 				    url: '/pages/message/good-friend-list'
 				});
 			},
-			openRun(){
+			openRun(ev){
 				uni.navigateTo({
-				    url: '../errands/index'
-				});
+					url: '../errands/index'
+				});				
 			},
 			showModal(e) {
 				this.modalName = e.currentTarget.dataset.target
@@ -104,5 +93,43 @@
 <style scoped>
 .cu-bar.tabbar{
 	bottom: calc(var(--window-bottom))
+}
+.new-text{
+	position: absolute;
+	top: -195rpx;
+	left: 20rpx;
+	width: 150rpx;
+	height: 160rpx;
+	background: #fff;
+	z-index: 100000;
+	border-radius: 20rpx;
+	-webkit-border-radius: 20rpx;
+	border: 1px solid #f3f3f3;
+	display: none;
+	color: #000;
+}
+.new-text.active{
+	display: block;
+}
+.new-text view{
+	width: 130rpx;
+	height: 80rpx;
+	line-height: 80rpx;
+	text-align: center;
+	margin:  0 auto;
+} 
+.new-index-cuIcon{
+	position: absolute;
+	bottom: -18rpx;
+	left: 35rpx;
+	width: 0;
+    height: 0;
+	border-top: 20rpx solid #fff;
+	border-left: 20rpx solid transparent;
+	border-right: 20rpx solid transparent; 
+	
+}
+.new-text view:first-of-type{
+	border-bottom: 1px solid #f3f3f3;
 }
 </style>
